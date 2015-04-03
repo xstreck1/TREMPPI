e8dfa89cf76c6f9e8897385149b7b0f95665c162
Adam Streck <adam.streck@gmail.com>
2015-04-03 20:01:38 +0200

    Added missing file.

--- tremppi_validate/data/automaton_builder.hpp
+++ tremppi_validate/data/automaton_builder.hpp
@@ -18,7 +18,7 @@
  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class AutomatonBuilder {
 	const RegInfos & reg_infos; ///< Model that holds the data.
-	const PropertyAutomaton & property;
+	const PropertyAutomaton & property_automaton;
 
 	vector<string> names; ///< Name of the i-th specie.
 	Levels maxes; ///< Maximal activity levels of the species.
@@ -26,40 +26,10 @@ class AutomatonBuilder {
 	Levels range_size; ///< Number of valid values for the species.
 
 	/**
-	 * Compute a vector of maximal levels and store information about states.
-	 */
-	void computeBoundaries() {
-		// Compute naive bounds.
-		for (const CompID ID : cscope(reg_infos)) {
-			// Maximal values of species
-			names.push_back(reg_infos[ID].name);
-			maxes.push_back(reg_infos[ID].max_activity);
-			mins.push_back(0);
-		}
-		// Add the steady state constraint
-		// names.push_back("#ss");
-		// maxes.push_back(1);
-		// mins.push_back(0);
-
-		// Add experiment constraints.
-		ConstraintParser * cons_pars = new ConstraintParser(reg_infos.size(), DataInfo::getMaxLevel(reg_infos));
-		cons_pars->addBoundaries(maxes, true);
-		cons_pars->applyFormula(DataInfo::getAllNames(reg_infos), property.experiment);
-		cons_pars->status();
-
-		// Compute refined boundaries.
-		mins = cons_pars->getBounds(false);
-		maxes = cons_pars->getBounds(true);
-		rng::transform(maxes, mins, back_inserter(range_size), [](const ActLevel max, const ActLevel min) {
-			return max - min + 1;
-		});
-	}
-
-	/**
 	 * Creates transitions from labelled edges of BA and passes them to the automaton structure.
 	 */
 	void addTransitions(AutomatonStructure & automaton, const StateID ID) const {
-		const PropertyAutomaton::Edges & edges = property.states[ID].edges;
+		const PropertyAutomaton::Edges & edges = property_automaton.states[ID].edges;
 
 		// Transform each edge into transition and pass it to the automaton
 		for (const PropertyAutomaton::Edge & edge : edges) {
@@ -74,15 +44,15 @@ class AutomatonBuilder {
 	}
 
 	/**
-	 * @brief setAutType sets type of the automaton based on the type of the property
+	 * @brief setAutType sets type of the automaton based on the type of the property_automaton
 	 */
 	void setAutType(AutomatonStructure & automaton) {
-		if (property.prop_type == "series")
-			if (property.states.back().stables_list.empty())
+		if (property_automaton.prop_type == "series")
+			if (property_automaton.states.back().stables_list.empty())
 				automaton.my_type = BA_finite;
 			else // Partial stable states require full check
 				automaton.my_type = BA_standard;
-		else if (property.prop_type == "cycle")
+		else if (property_automaton.prop_type == "cycle")
 			automaton.my_type = BA_standard;
 		else
 			throw runtime_error("Type of the verification automaton is not known.");
@@ -91,16 +61,22 @@ class AutomatonBuilder {
 	vector<CompID> transformStables(const vector<string> & stable_names) {
 		vector<CompID> stables(stable_names.size());
 
-		transform(WHOLE(stable_names), begin(stables), [this](const string & stable_name) { 
-			return DataInfo::getCompID(reg_infos, stable_name); 
+		transform(WHOLE(stable_names), begin(stables), [this](const string & stable_name) {
+			return DataInfo::getCompID(reg_infos, stable_name);
 		});
 
 		return stables;
 	}
 
 public:
-	AutomatonBuilder(const RegInfos & _reg_infos, const PropertyAutomaton & _property) : reg_infos(_reg_infos), property(_property) {
-		computeBoundaries();
+	AutomatonBuilder(const RegInfos & _reg_infos, const PropertyAutomaton & _property_automaton) : reg_infos(_reg_infos), property_automaton(_property_automaton) {
+		pair<Levels, Levels> bounds = PropertyHelper::getBounds(reg_infos, property_automaton);
+		mins = bounds.first; maxes = bounds.second;
+		rng::transform(maxes, mins, back_inserter(range_size), [](const ActLevel max, const ActLevel min) {return max - min + 1; });
+
+		for (const CompID ID : cscope(reg_infos)) {
+			names.push_back(reg_infos[ID].name);
+		}
 	}
 
 	/**
@@ -109,13 +85,13 @@ public:
 	AutomatonStructure buildAutomaton() {
 		AutomatonStructure automaton;
 		setAutType(automaton);
-		const size_t state_count = property.states.size();
+		const size_t state_count = property_automaton.states.size();
 		size_t state_no = 0;
 
 		// List throught all the automaton states
-		for (StateID ID : cscope(property.states)) {
+		for (StateID ID : cscope(property_automaton.states)) {
 			// Convert names into IDs
-			const vector<string> & stable_names = property.states[ID].stables_list;
+			const vector<string> & stable_names = property_automaton.states[ID].stables_list;
 			vector<CompID> stables;
 			if (stable_names.size() == 1 && stable_names[0] == "#ALL")
 				stables = transformStables(DataInfo::getAllNames(reg_infos));
@@ -123,7 +99,7 @@ public:
 				stables = transformStables(stable_names);
 
 			// Fill auxiliary data
-			automaton.addState(ID, property.states[ID].final, stables);
+			automaton.addState(ID, property_automaton.states[ID].final, stables);
 			// Add transitions for this state
 			addTransitions(automaton, ID);
 		}
