04be2cd250b4c95eeb1c797828fd4537f599104b
Adam Streck <adam.streck@gmail.com>
2015-06-01 16:07:52 +0200

    Added missing file.

--- native/tremppi_regulations/compute/regulatory_graph.hpp
+++ native/tremppi_regulations/compute/regulatory_graph.hpp
@@ -37,7 +37,7 @@ namespace RegulatoryGraph {
 		// Compute the correlation and frequency
 		for (const auto & regulator : reg_info.regulators) {
 			// Obtain the id of the regulator from the order in the regulator list and prepare the results
-			size_t reg_no = DataInfo::getRegulatorI(regulator.first, reg_info);
+			size_t reg_no = DataInfo::RegIDToRegNo(reg_info, regulator.first);
 			result.reg_corr.insert({ regulator.first, vector<double>(regulator.second.size(), 0.0) });
 			auto & reg_corr = result.reg_corr[regulator.first];
 			result.reg_freq.insert({ regulator.first, vector<double>(regulator.second.size(), 0.0) });
@@ -51,6 +51,46 @@ namespace RegulatoryGraph {
 				reg_freq[edge_no] += isEdgeFunctional(context_values, edge_no);
 			}
 		}
+	}
+
+
+	map<CompID, vector<vector<size_t>>> getColumnsOfThresholds(const RegInfo & reg_info)
+	{
+		map<CompID, vector<vector<size_t>>> result;
+
+		for (auto & regulator : reg_info.regulators) {
+			vector<vector<size_t> > columns(regulator.second.size() + 1);
+			const size_t reg_i = DataInfo::RegIDToRegNo(reg_info, regulator.first);
+			map<ActLevel, size_t> threshold_to_index = DataConv::getThresholdToIndex(regulator.second);
+
+			// Distribute the column indices based on the value of the threshold in the specific column
+			for (auto & column : reg_info.columns) {
+				const auto & trhs = DataConv::getThrsFromContext(column.second);
+				ActLevel trh = trhs[reg_i];
+				size_t i = threshold_to_index[trh];
+				columns[i].push_back(column.first);
+			}
+
+			result.insert({ regulator.first, move(columns) });
+		}
+
+		return result;
+	}
+
+	map<CompID, vector<vector<size_t>>> getColumnsOfEdges(const map<CompID, vector<vector<size_t>>> & columns_of_thresholds) {
+		map<CompID, vector<vector<size_t> > > result;
+
+		for (const auto & source_columns : columns_of_thresholds) {
+			vector<vector<size_t>> edge_columns(source_columns.second.size() - 1);
+
+			for (size_t trhs_column_i = 0; trhs_column_i < edge_columns.size(); trhs_column_i++) {
+				edge_columns[trhs_column_i] = source_columns.second[trhs_column_i] + source_columns.second[trhs_column_i + 1];
+			}
+
+			result.insert({ source_columns.first, edge_columns });
+		}
+
+		return result;
 	}
 
 	// Divide the number of parametrizations in total by the number of parametrizations sharing the same regulatory function
@@ -75,14 +115,14 @@ namespace RegulatoryGraph {
 		map<CompID, map<ActLevel, size_t> > threshold_to_index; // Mapper from thresholds to their order, for each regulator
 		for (const auto & regulator : reg_info.regulators) {
 			// Obtain the id of the regulator from the order in the regulator list
-			size_t reg_no = DataInfo::getRegulatorI(regulator.first, reg_info);
+			size_t reg_no = DataInfo::RegIDToRegNo(reg_info, regulator.first);
 			reg_values.emplace_back(DataConv::getThrFromContexts(columns_list, reg_no));
 			threshold_to_index.insert({ regulator.first, DataConv::getThresholdToIndex(regulator.second) });
 			// Compute the expected frequency for this regulator
 			result.expected_freq[regulator.first] = computeExpectedFreq(reg_info, regulator.first);
 		}
-		auto columns_dist = DataInfo::getColumnsOfThresholds(reg_info);
-		auto edge_dist = DataConv::getColumnsOfEdges(columns_dist);
+		auto columns_dist = getColumnsOfThresholds(reg_info);
+		auto edge_dist = getColumnsOfEdges(columns_dist);
 
 		// Compute relation to each regulation of this component, step by database rows
 		for (const auto & row : qry) 
@@ -100,32 +140,5 @@ namespace RegulatoryGraph {
 		normalize(result.reg_freq);
 
 		return result;
-	}
-
-	//
-	map<CompID, vector<double> > substractEdgeData(const map<CompID, vector<double> > & A, const map<CompID, vector<double> > & B) {
-		map<CompID, vector<double> > result;
-
-		for (auto & reg_A : A) {
-			auto & reg_B = B.at(reg_A.first);
-			vector<double> diff(reg_A.second.size());
-			for (const size_t val_i : cscope(reg_A.second))
-				diff[val_i] = reg_A.second[val_i] - reg_B[val_i];
-			result.insert({ reg_A.first, diff });
-		}
-
-		return result;
-	}
-
-	//
-	RegsData diff(const RegsData & sel, const RegsData & cmp) {
-		RegsData dif;
-		for (const CompID ID : cscope(sel)) {
-			dif.emplace_back(RegData({ sel[ID].info }));
-			dif[ID].reg_corr = substractEdgeData(sel[ID].reg_corr, cmp[ID].reg_corr);
-			dif[ID].reg_freq = substractEdgeData(sel[ID].reg_freq, cmp[ID].reg_freq);
-			dif[ID].expected_freq = sel[ID].expected_freq;
-		}
-		return dif;
 	}
 }
