1a5be3f268a64268ffc68668de25c49ace8a4ad1
Adam Streck <adam.streck@gmail.com>
2015-06-16 15:45:31 +0200

    Minor updates.

---
+++ native/tests/sign_tests.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "common_tests_data.hpp"
+
+int basic_sign_test() {
+	vector<string> arguments = { "--path", bfs::absolute(TremppiSystem::HOME_PATH / TEST_FOLDER / TEST_PROJECT).string() };
+	ArgPtr arg_ptr(arguments);
+	return (tremppi_sign(arg_ptr.getArgc(), arg_ptr.getArgv()));
+}
+
